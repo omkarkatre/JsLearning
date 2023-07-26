61edb1241fb7d1e19294669322d0f9556c821efb
omkarkatre <omkarkatre003@gmail.com>
2023-07-26 19:16:56 +0530

    sawp
    sawp

---
+++ swapVariables.js
@@ -0,0 +1,29 @@
+console.log("--------Before Swap-----------");
+var Sweety="Sweety";
+var Cutie="Cutie";
+console.log("Sweety:Sweety");
+console.log("Cutie:Cutie");
+var temp=Sweety;
+Sweety=Cutie;
+Cutie=temp;
+console.log("----------After Swap----------------");
+
+console.log("Sweety:",Sweety);
+console.log("Cutie:",Cutie);
+var num1=100;
+var num2=200;
+var num3=300;
+console.log("------------Before swap-----------");
+console.log("num1:",num1);
+console.log("num2:",num2);
+console.log("num3:",num3);
+console.log("------------After swap-----------");
+temp=num1;
+num1=num2;
+num2=num3;
+num3=temp;
+console.log("num1:",num1);
+console.log("num2:",num2);
+console.log("num3:",num3);
+
+
