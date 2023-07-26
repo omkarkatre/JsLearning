dadba676fa0d8d35c262c78dabea8df1343d57fb
omkarkatre <omkarkatre003@gmail.com>
2023-07-26 19:17:18 +0530

    variable
    variable

---
+++ variableAssign.js
@@ -0,0 +1,27 @@
+console.log("-----------Value before intailization----------");
+var myFullname;
+console.log("Type of myFullname:",myFullname);
+console.log("-----------Value After intailization----------");
+
+var myFullname = "omkar katre";
+console.log("First & last name:",myFullname);
+var myFullName1= "omkar Suhas katre";
+console.log("Full name of student:",myFullName1);
+console.log("------------------------------");
+//var myLove;
+myLove="I love only JS";
+console.log("My Love:",myLove);
+console.log("---------------------");
+firstName="Omkar";
+lastName="Katre";
+age="23";
+address="AT post-Kole,Tal=Karad,Dist=Satara";
+Pincode=415103;
+isMarried=false;
+console.log("FirstName:",firstName);
+console.log("lastName:",lastName);
+console.log("age:",age);
+console.log("address:",address);
+console.log("Pincode:",Pincode);
+console.log("----------------------------");
+
