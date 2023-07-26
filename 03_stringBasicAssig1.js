b8fe0cc7da4fa7e51aa49deececa056c1d629794
omkarkatre <omkarkatre003@gmail.com>
2023-07-26 19:16:04 +0530

    string
    string

---
+++ 03_stringBasicAssig1.js
@@ -0,0 +1,19 @@
+function stringBasics(params) {
+    console.log("-------Step1--------------");
+    
+    console.log("My dream comapny is Infosys");
+    console.log("-------Step2--------------");
+    Myhobby1="Playing Cricket";
+    Myhobby2="Watching movie";
+    Myhobby3="Tracking";
+    Hobby1=Myhobby1.length;
+    Hobby2=Myhobby2.length;
+    Hobby3=Myhobby3.length;
+    Sum=Myhobby1.length+Myhobby2.length+Myhobby3.length;
+    console.log(`My first Hobby is:${Myhobby1}`);
+    console.log(`My Second Hobby is:${Myhobby2}`);
+    console.log(` My Third Hobby3 is:${Myhobby3}`);
+    console.log(`Sum of total number of character in first,Second & Third hobby is:${Sum} `);
+}
+stringBasics();
+
