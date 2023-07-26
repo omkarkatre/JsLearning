566241b93c0c91456bd3694b9062272d6f7945e0
omkarkatre <omkarkatre003@gmail.com>
2023-07-26 19:17:01 +0530

    Create typeOf.js

---
+++ typeOf.js
@@ -0,0 +1,28 @@
+console.log("---------------Before intalization-------------- ");
+var BankName;
+console.log("Type of BankName:",typeof BankName);
+console.log("---------------After intalization-----------------");
+BankName="SBI Bank";
+console.log("Name of bank:",BankName);
+console.log("----------------Before Marks  intalization----------------");
+var marks;
+console.log("Type of marks:", typeof marks);
+console.log("----------------After Marks  intalization----------------");
+marks= "90%"
+console.log("MARKS:",marks);
+console.log("Type of MARKS:",typeof marks);
+console.log("---------------------------");
+var isWorking;
+console.log("Before the intialization valve:",isWorking);
+console.log("Before the intialization valve:",typeof isWorking);
+console.log("--------------------------------");
+isWorking=true;
+console.log("After the intialization valve:",isWorking);
+console.log("After the intialization valve:",typeof isWorking);
+var totalCount="Hundred and seven"
+console.log(" TotalCount:",totalCount);
+console.log(" Type of TotalCount:",typeof totalCount);
+totalCount=107;
+console.log("TotalCount:",totalCount);
+console.log("TotalCount:",typeof totalCount);
+console.log("----------------------------------------");
