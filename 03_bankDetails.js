04ce514814d3c98ff97c37c311fc6830d8636415
omkarkatre <omkarkatre003@gmail.com>
2023-07-26 19:15:00 +0530

    Create 03_bankDetails.js
    basic

---
+++ 03_bankDetails.js
@@ -0,0 +1,13 @@
+console.log("-------------Normal function----------");
+function bankDetails(bankName,accountNo,location,pinCode) {
+    console.log("Name of Bank: ",bankName);
+    console.log("Number of account:",accountNo);
+    console.log("Location of Bank:",location);
+    console.log("Pincode of city:",pinCode);
+    console.log("----------------------------------------------------");
+    
+    
+}
+bankDetails("CITI Bank",456433345,"Pune",431202);
+bankDetails("Axis Bank",456433345,"Mumbai",441202);
+bankDetails("HDFC Bank",456433345,"Pune",631202,"open");
