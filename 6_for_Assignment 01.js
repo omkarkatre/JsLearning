256b29f0f125817fc5af0d8f67a2876bb36883f3
omkarkatre <omkarkatre003@gmail.com>
2023-08-01 21:58:53 +0530

    Create 6_for_Assignment 01.js

---
+++ 6_for_Assignment 01.js
@@ -0,0 +1,31 @@
+console.log(`----------------Print number from 5 to 15 by incementing by 1-------------------`);
+for (let index = 5; index <=15; index++) {
+    console.log(`${index}`);
+    
+}
+console.log(`----------------- Print number from 50 yo 40 by decrementing by 1------------------`);
+for (let index = 50; index >40; index--) {
+    console.log(`${index}`);
+}
+console.log(`----------------- first 15 odd numbers------------------`);
+for (let index = 1; index <30 ; index=index+2) {
+    console.log(`${index}`);
+}
+console.log(`-----------------first 10 even numbers -----------------`);
+for (let index = 0; index < 20; index=index+2) {
+   console.log(`${index}`);
+}
+console.log(`-----------------WAP print table of 5  -----------------`);
+for (let index = 5; index <=50; index=index+5) {
+    console.log(`${index}`);
+}
+console.log(`-----------------WAP print table of 10  -----------------`);
+for (let index = 10; index <=100; index=index+10) {
+    console.log(`${index}`);
+    
+}
+console.log(`----------------- Print number from 100 to 10 by decrementing by 10------------------`);
+for (let index = 100; index >=10; index=index-10) {
+    console.log(`${index}`);
+}
+
