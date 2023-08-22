362d6fe7aae94d58be4fb809257ce26d726f067d
omkarkatre <omkarkatre003@gmail.com>
2023-08-22 20:21:01 +0530

    arrowfunction
    arrowfunction

---
+++ 22_arrowFunctionAssigA.js
@@ -0,0 +1,15 @@
+let wish=()=>{
+    console.log(`Good Morning,Today is Tuesday`);
+}
+wish();
+let multiply=(num1,num2,num3=1)=>{
+    console.log(`Mulyiplication of nummbers:`,num1*num2*num3);
+}
+multiply(5,5,2);
+multiply(10,4);
+let addition =(arg1,arg2,arg3,arg4,arg5)=>{
+    console.log(`Addition of arguments:`,arg1+arg2+arg3+arg4+arg5);
+    
+}
+addition(100,100,200,349,756)
+addition(`I am `,`learning`,`ES6`,`feature`,`in depth`);
